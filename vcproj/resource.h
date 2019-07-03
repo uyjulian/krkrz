7030b6d654940e44f1a7f926bd5d6afe4c696920
uyjulian <uyjulian@gmail.com>
2019-07-03 10:51:22 -0500

    Hardcode system initialization script

--- vcproj/resource.h
+++ vcproj/resource.h
@@ -4,5 +4,3 @@
 //
 #define IDI_TVPWIN32                    107
 #define IDC_TVPWIN32                    109
-#define IDR_OPTION                      139
-#define IDR_SYS_INIT_SCRIPT             143
