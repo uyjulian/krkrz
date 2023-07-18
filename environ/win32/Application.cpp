00b168230b82885a5a66040c45856d5dbfe33c18
uyjulian <uyjulian@gmail.com>
2023-07-18 09:53:01 -0500

    Fix pointer casting

--- environ/win32/Application.cpp
+++ environ/win32/Application.cpp
@@ -464,7 +464,7 @@ void tTVPApplication::CloseConsole() {
 
 void tTVPApplication::PrintConsole( const tjs_char* mes, unsigned long len, bool iserror ) {
 	HANDLE hStdOutput = ::GetStdHandle(iserror ? STD_ERROR_HANDLE : STD_OUTPUT_HANDLE);
-	if (hStdOutput > 0) {
+	if (hStdOutput != 0) {
 		DWORD mode;
 		if (GetConsoleMode(hStdOutput, &mode)) {
 			// 実コンソール
